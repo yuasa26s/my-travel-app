2e30c75d654eb0ccc1d3cd6f2961c62eba80ec20
mura-2025 <aya3754.h@gmail.com>
2026-02-17 21:57:18 +0900

    ログイン機能追加

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// # TODO: fairebaseコンソールから取得して入力のこと！
 const firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
